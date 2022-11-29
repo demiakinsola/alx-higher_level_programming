ccefc7a88efdf3fbab657d90281a5ea1b177a943
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-11-29 13:05:24 -0800

    Wrote a program that prints the last digit of a number.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-num_str = repr(number) #get the string rep. of the number
-# Acces the last string of the digit string
+num_str = repr(number)
+# Get the string rep. of the number
+# Access the last string of the digit string
 last_digit_str = num_str[-1]
-# Covert the last digit string to an integer
+# Convert the last digit string to an integer
 last_digit = int(last_digit_str)
 if last_digit > 5:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
 elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 else:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    if number < 0:
+        print(f"Last digit of {number} is {-1 * last_digit}", end="")
+        print(f" and is less than 6 and not 0")
+    else:
+        print(f"Last digit of {number} is {last_digit}", end="")
+        print(f" and is less than 6 and not 0")
