6d0970a8acf7e8ab083da109a3ed9008bee6ae41
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-12-01 12:49:33 -0800

    Fixed import line.

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 # this guard prevents the code from executing when imported.
 if __name__ == "__main__":
 
-    import calculator_1
+    from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
