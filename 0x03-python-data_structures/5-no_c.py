96a1af895212ede728075af700c889cbcc531f3c
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-12-05 12:44:44 -0800

    Fixed Pycodestyle errors.

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,8 +3,6 @@
 def no_c(my_string):
     new_string = ""
     for i in range(0, len(my_string)):
-        if my_string[i] != 'C'  and my_string[i] != 'c':
+        if my_string[i] != 'C' and my_string[i] != 'c':
             new_string = new_string + my_string[i]
     return new_string
-            
-    
