50f565f441bb0e095db150095c343338470fa390
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-12-08 00:06:39 -0800

    Fixed pycodestyle errors.

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    for key, value in sorted(a_dictionary.items(), key=lambda x: x[0]): 
-# This is to get the keys and values sorted by x[0](i.e. keys)
+    for key, value in sorted(a_dictionary.items(), key=lambda x: x[0]):
+        # This is to get the keys and values sorted by x[0](i.e. keys)
         print('{}: {}'.format(key, value))
